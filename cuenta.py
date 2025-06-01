a3ec49f8a3950873e39684559e71c2c70d0f9b4c
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2025-05-31 19:17:08 -0600

    Refactor Cuenta class: streamline account creation by adding input validation, improve display formatting, and enhance saldo method for better error handling.

--- cuenta.py
+++ cuenta.py
@@ -8,10 +8,8 @@ class Cuenta:
         print(f"<<DATOS DE LA CUENTA>>")
         self.__nc = nc
         print(f"No. cuenta: {nc}")
-        self.__nm = self.obd.cadena("Nombre de la cuenta").upper()
-        self.__suc = 0
-        while self.__suc < 1 or self.__suc > 10:
-            self.__suc = self.obd.entero("Sucursal")
+        self.__nm = self.obd.cadena("Nombre de la cuenta").upper().strip()
+        self.__suc = self.obd.entero("Sucursal")
         self.__fa = FechaHora().fecha()
         print(f"Fecha de apertura: {self.__fa}")
         self.__sal = 0
@@ -24,46 +22,50 @@ class Cuenta:
 
     def títulos(self):
         obf = Formato()
-        print(f"| {obf.izq('NO.', 3)} | {obf.cen('NOMBRE', 30)} | {obf.der('SUC', 3)} | {obf.izq('FEC. AP', 12)} | {obf.der('SALDO', 15)} | {obf.cen('ESTADO', 10)} | {obf.der('No.C Asociado', 20 )} |")
+        print(f"| {obf.izq('NO.', 3)} | {obf.cen('NOMBRE', 30)} | {obf.der('SUC', 3)} | {obf.izq('FEC. AP.', 12)} | {obf.der('SALDO', 15)} | {obf.cen('ESTADO', 10)}")
 
     def mostrar(self):
         obf = Formato()
-        print(f"| {obf.izq(self.__nc, 3)} | {obf.cen(self.__nm, 30)} | {obf.der(self.__suc, 3)} | {obf.izq(self.__fa, 12)} | {obf.der(obf.mon(self.__sal), 15)} | {obf.cen('ACTIVO' if self.__act else 'INACTIVO', 10)} | {obf.der(self.__ncl, 20 )} |")
+        print(f"| {obf.izq(self.__nc, 3)} | {obf.cen(self.__nm, 30)} | {obf.der(self.__suc, 3)} | {obf.izq(self.__fa, 12)} | {obf.der(self.__sal, 15)} | {obf.cen('ACTIVO' if self.__act else 'INACTIVO', 10)}")
 
-    def movimiento(self, mon, tip):
-        si = self.__sal
+    def saldo(self, mon, tip):
         sal = self.__sal
         if tip:
             mon *= -1
         sal += mon
         if sal == 0:
-            print('Cuenta inactiva, saldo cero')
             self.__act = 0
             self.__sal = sal
+            return 0, 0
         elif sal < 0:
-            self.__sal = si
-            raise Exception(
-                'El saldo no puede ser negativo, operación cancelada')
+            print('No se realizo la operación')
+            self.__sal = 0
+            return 0, 0
         else:
             self.__act = 1
             self.__sal = sal
-        return self.__sal
+            return 1, self.__ncl
 
     def modificar(self):
-        obm = Menu('Menu de Cliente', [
-                   'Nombre de la cuenta', 'Sucursal', 'Fecha'])
+        print("\n<<MODIFICAR DATOS DE LA CUENTA>>")
+        print("<<DATOS ACTUALES>>")
+        self.títulos()
+        self.mostrar()
+        obm = Menu(f"CUENTA {self.__ncl}", [
+                   "NOMBRE", 'SUCURSAL', 'FECHA DE APERTURA'])
         op = 0
         while op != obm.salir():
             match op := obm.opcion():
                 case 1:
-                    self.__nm = self.obd.cadena("Nombre de la cuenta").upper()
+                    self.__nm = self.obd.cadena(
+                        "Nombre de la cuenta").upper().strip()
                 case 2:
                     self.__suc = 0
                     while self.__suc < 1 or self.__suc > 10:
                         self.__suc = self.obd.entero("Sucursal")
                 case 3:
-                    obfe = FechaHora()
-                    self.__fa = obfe.fecha()
+                    obf = FechaHora()
+                    self.__fa = obf.fecha()
                     print('Fecha actualizada')
 
     def nc(self):
